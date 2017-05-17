98ec7f6adafc037d1d720ebc9d68cc5365b9441e
lovealamo <631820097@qq.com>
2017-05-17 08:57:27 +0800

    refine spaces
    refine some space and code.

--- youeryuanmap/header.php
+++ youeryuanmap/header.php
@@ -5,6 +5,6 @@
 	<link rel="stylesheet" type="text/css" href="css/app.css">
 	<title>地图展示</title>
 	<script type="text/javascript" src="http://api.map.baidu.com/api?v=2.0&ak=6693136ece2aeb0fd6ef97b2d2bcc9ba"></script>
-        <script type="text/javascript" src="http://api.map.baidu.com/library/AreaRestriction/1.2/src/AreaRestriction_min.js"></script>
+    <script type="text/javascript" src="http://api.map.baidu.com/library/AreaRestriction/1.2/src/AreaRestriction_min.js"></script>
 </head>
 <body>
